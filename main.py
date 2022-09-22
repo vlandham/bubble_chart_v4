b6fa86f89d45a228ef01dc09a2c8f1f28fdb34a0
Beto Martinez <luismartinez@getbento.com>
2022-09-22 13:08:22 -0500

    randomizing order values

--- main.py
+++ main.py
@@ -1,54 +1,37 @@
-from flask import Flask, render_template, send_from_directory, json
+import uuid
+import random
+
+from flask import Flask, render_template, json
 
 
 app = Flask(__name__)
 
-orders = [
-    {"id": 1, "amount": 10.00, 'state': 'ready', 'date': '09-22-2022', 'bentobox_revenue': 1.00, 'restaurant_revenue': 9.00},
-    {"id": 2, "amount": 25.90, 'state': 'ready', 'date': '09-22-2022', 'bentobox_revenue': 2.59, 'restaurant_revenue': 23.41}
-]
+POSSIBLE_STATES = ('paid', 'unpaid')
 
-money = [
-    {
-        'grant_title': 'New Mexico Business Roundtable',
-    'id': 1,
-'organization': 'New Mexico Business Roundtable for Educational Excellence',
-'total_amount': '5000',
-'group': 'low',
-'Grant start date': '2/4/2010',
-'start_month': '2',
-'start_day': '4',
-'start_year': '2010'
-    },
-    {
-        'grant_title': 'LA NSC Match',
-    'id': '2',
-'organization': 'Trustees of Dartmouth College',
-'total_amount': '27727',
-'group': 'low',
-'Grant start date': '8/3/2009',
-'start_month': '8',
-'start_day': '3',
-'start_year': '2009'
-    },
-    {
-        'grant_title': 'Education Equity Agenda: Improving Native Student Graduation Rates: Policy Recommendations on High School Reform',
-    'id': '114',
-'organization': 'National Indian Education Association',
-'total_amount': '520446',
-'group': 'medium',
-'Grant start date': '8/31/2009',
-'start_month': '8',
-'start_day': '31',
-'start_year': '2009'
-    },
-]
+
+def _get_random_id():
+    return uuid.uuid4()
+
+
+def _get_random_price():
+    return round(random.uniform(33.33, 66.66), 2)
+
+
+def _create_order():
+    return {
+        "id": _get_random_id(),
+        "amount": _get_random_price(),
+        'state': 'ready',
+        'date': '09-22-2022',
+        'bentobox_revenue': 1.00,
+        'restaurant_revenue': 9.00
+    }
 
 
 @app.route('/orders/', defaults={'page': 0})
 @app.route('/orders/<int:page>', methods=['GET'])
 def get_orders(page: int):
-    return json.dumps(money)
+    return json.dumps([_create_order()])
 
 
 @app.route("/")
