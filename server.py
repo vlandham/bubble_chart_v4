f3dc999bba7eb759bfbbf93f1e03d63c34b73c19
Beto Martinez <luismartinez@getbento.com>
2022-09-22 11:37:36 -0500

    add a flask server

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-from flask import Flask, json
+from src.data_generator.api import api
 
-api = Flask(__name__)
 
 if __name__ == '__main__':
     api.run('127.0.0.1', 3000, True)
--- src/data_generator/api.py
+++ src/data_generator/api.py
@@ -1,11 +1,18 @@
-import json
-from server import api
+from flask import Flask, json, send_from_directory
+
+api = Flask(__name__)
 
 orders = [
     {"id": 1, "amount": 10.00, 'state': 'ready', 'date': '09-22-2022', 'bentobox_revenue': 1.00, 'restaurant_revenue': 9.00},
     {"id": 2, "amount": 25.90, 'state': 'ready', 'date': '09-22-2022', 'bentobox_revenue': 2.59, 'restaurant_revenue': 23.41}
 ]
 
+
 @api.route('/orders', methods=['GET'])
 def get_orders():
     return json.dumps(orders)
+
+
+@api.route('/', methods=['GET'])
+def index():
+    return send_from_directory('.', '/app/index.html')
