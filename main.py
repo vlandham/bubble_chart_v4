6b5e65d72044e4685fb9880c5b113f0072be3e0f
Beto Martinez <luismartinez@getbento.com>
2022-09-22 13:14:48 -0500

    randomizing order values

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 import uuid
 import random
+from decimal import Decimal
 
 from flask import Flask, render_template, json
 
@@ -17,14 +19,38 @@ def _get_random_price():
     return round(random.uniform(33.33, 66.66), 2)
 
 
+def str_time_prop(start, end, time_format, prop):
+    """Get a time at a proportion of a range of two formatted times.
+
+    start and end should be strings specifying times formatted in the
+    given format (strftime-style), giving an interval [start, end].
+    prop specifies how a proportion of the interval to be taken after
+    start.  The returned time will be in the specified format.
+    """
+
+    stime = time.mktime(time.strptime(start, time_format))
+    etime = time.mktime(time.strptime(end, time_format))
+
+    ptime = stime + prop * (etime - stime)
+
+    return time.strftime(time_format, time.localtime(ptime))
+
+
+def random_date(start, end, prop):
+    return str_time_prop(start, end, '%m/%d/%Y', prop)
+
+
+
 def _create_order():
+    resto_rev = _get_random_price()
+    amount = _get_random_price()
     return {
         "id": _get_random_id(),
-        "amount": _get_random_price(),
-        'state': 'ready',
-        'date': '09-22-2022',
-        'bentobox_revenue': 1.00,
-        'restaurant_revenue': 9.00
+        "amount":amount,
+        'state': random.choice(POSSIBLE_STATES),
+        'date': random_date("1/1/2022", "1/1/2023", random.random()),
+        'bentobox_revenue': "{:.2f}".format(round(abs(amount - resto_rev), 2)),
+        'restaurant_revenue': resto_rev
     }
 
 
