cf6a537141809878576ec0b1ec142e8f0e1118ea
Andrew Craft <drewtcraft@gmail.com>
2022-09-23 09:01:37 -0400

    working for order data

--- static/bubbles_2.js
+++ static/bubbles_2.js
@@ -1,35 +1,13 @@
-var data1 = [
-    {year1: 20, year2: 1, year3: 3, label: "a"},
-    {year1: 3, year2: 4, year3: 5, label: "b"},
-    {year1: 5, year2: 9, year3: 7, label: "c"},
-    {year1: 8, year2: 16, year3: 11, label: "d"},
-    {year1: 13, year2: 25, year3: 13, label: "e"},
-    {year1: 21, year2: 36, year3: 17, label: "f"},
-    {year1: 34, year2: 1, year3: 19, label: "g"},
-    {year1: 2, year2: 4, year3: 23, label: "h"},
-    {year1: 3, year2: 9, year3: 29, label: "i"},
-    {year1: 5, year2: 16, year3: 31, label: "j"},
-    {year1: 8, year2: 25, year3: 37, label: "k"},
-    {year1: 13, year2: 36, year3: 3, label: "l"},
-    {year1: 21, year2: 1, year3: 5, label: "m"}
-];
+const data1 = [
 
-let data2 = [
-    {year1: 2, year2: 1, year3: 3, label: "a"},
-    {year1: 3, year2: 4, year3: 5, label: "b"},
-    {year1: 5, year2: 9, year3: 7, label: "c"},
-    {year1: 8, year2: 16, year3: 11, label: "d"},
-    {year1: 13, year2: 25, year3: 13, label: "e"},
-    {year1: 2, year2: 36, year3: 17, label: "f"},
-    {year1: 34, year2: 1, year3: 19, label: "g"},
-    {year1: 2, year2: 4, year3: 23, label: "h"},
-    {year1: 3, year2: 9, year3: 29, label: "i"},
-    {year1: 5, year2: 16, year3: 31, label: "j"},
-    {year1: 8, year2: 25, year3: 37, label: "k"},
-    {year1: 13, year2: 36, year3: 3, label: "l"},
-    {year1: 21, year2: 1, year3: 5, label: "m"},
-    {year1: 21, year2: 1, year3: 5, label: "z"}
-];
+    {"id": 1, "amount": 10.00, 'state': 'unpaid', 'date': '09-22-2022', 'bentobox_revenue': 1.00, 'restaurant_revenue': 9.00},
+    {"id": 2, "amount": 25.90, 'state': 'unpaid', 'date': '09-22-2022', 'bentobox_revenue': 2.59, 'restaurant_revenue': 23.41}
+]
+
+const data2 = [
+    {"id": 1, "amount": 10.00, 'state': 'unpaid', 'date': '09-22-2022', 'bentobox_revenue': 1.00, 'restaurant_revenue': 9.00},
+    {"id": 2, "amount": 25.90, 'state': 'paid', 'date': '09-22-2022', 'bentobox_revenue': 2.59, 'restaurant_revenue': 23.41}
+]
 
 function sickChart(data) {
     let width = 1180;
@@ -42,7 +20,7 @@ function sickChart(data) {
         .attr("height", height);
 
     var maxAmount = d3.max(data, function (d) {
-        return d.year1;
+        return d.amount;
     });
 
     // Sizes bubbles based on area.
@@ -56,11 +34,9 @@ function sickChart(data) {
         .domain([0, 10, 37])
         .range(["#c7e9b4", "#41b6c4", "#253494"]);
 
-    var year = "year1";
-
     var simulation = d3.forceSimulation()
         .force("x", d3.forceX(function (d) {
-            if (parseFloat(d[year]) > 5) {
+            if (d.state === 'unpaid') {
                 return width / 4;
             } else {
                 return width / 4 * 3;
@@ -69,17 +45,17 @@ function sickChart(data) {
         .force("y", d3.forceY(height / 2).strength(0.05))
         .force("collide", d3.forceCollide()
             .radius(function (d) {
-                return radiusScale(d[year])
+                return radiusScale(d.amount)
             }));
 
     var bubbles = svg.selectAll("circle")
         .data(data)
         .enter().append("circle")
         .attr("r", function (d) {
-            return radiusScale(d[year])
+            return radiusScale(d.amount)
         })
         .attr("fill", function (d) {
-            return colorScale(d[year]);
+            return colorScale(d.amount);
         });
 
     simulation.nodes(data)
@@ -98,13 +74,11 @@ function sickChart(data) {
         if (err) {
             return console.error(err);
         }
-            console.log(bubbles.data())
-            const old = new Map(bubbles.data().map(d => [d.label, d]));
-            console.log(newData);
-            data = newData.map(d => Object.assign(old.get(d.label) || {}, d));
+            const old = new Map(bubbles.data().map(d => [d.id, d]));
+            data = newData.map(d => Object.assign(old.get(d.id) || {}, d));
 
             maxAmount = d3.max(data, function (d) {
-                return d.year1;
+                return d.amount;
             });
             radiusScale = d3.scalePow()
                 .exponent(0.5)
@@ -128,9 +102,9 @@ function sickChart(data) {
             simulation.force("collide").initialize(data);
 
             bubbles.attr("r", function (d) {
-                return radiusScale(d.year1)
+                return radiusScale(d.amount)
             }).attr("fill", function (d) {
-                return colorScale(d[year]);
+                return colorScale(d.amount);
             })
         }
 }
